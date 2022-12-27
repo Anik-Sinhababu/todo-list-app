ca5ed2efb5abd66fbb69f588d50039ad0fa72bdc
ANIK <sinhababuanik@gmail.com>
2022-12-27 20:43:51 +0530

    implemented the edit button in todos app

--- GUI.py
+++ GUI.py
@@ -4,22 +4,35 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a To-Do Item", text_color="black", background_color="lavender blush2")
 input_box = sg.InputText(tooltip="Enter a todo item", key="todo")
 button = sg.Button('Add', button_color="black")
-window = sg.Window('MY TO_DO APP', layout=[[label], [input_box, button]],
+list_box = sg.Listbox(values=get_todos(), key='todos', enable_events=True, size=[120, 20], background_color="lavender")
+edit_button = sg.Button("Edit")
+window = sg.Window('MY TO_DO APP', layout=[[label], [input_box, button],
+                                           [list_box, edit_button]],
                    background_color="gray20",
-                   font=('Dina', 13))
-sg.TITLEBAR_MAXIMIZE_KEY
+                   font=('Dina', 14))
+
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
     match event:
         case 'Add':
             todos = get_todos()
-            print(todos)
             new_todo = values['todo'] + '\n'
-            print(new_todo)
             todos.append(new_todo)
             write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case 'Edit':
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+            todos = get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case 'todos':
+            input_box = window['todo'].update(value=values['todos'][0])
+
         case sg.WIN_CLOSED:
             break
 
