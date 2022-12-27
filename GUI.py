3b660b25f00f88c9c9ee63010e9ccfb4a48ba352
ANIK <sinhababuanik@gmail.com>
2022-12-27 17:30:40 +0530

    implemented the add button in todos app

--- GUI.py
+++ GUI.py
@@ -1,9 +1,26 @@
-import functions
+from functions import get_todos, write_todos
 import PySimpleGUI as sg
 
-label = sg.Text("Type in a To-Do Item", text_color="white", background_color="indian red")
-input_box = sg.InputText(tooltip="Enter a todo item")
+label = sg.Text("Type in a To-Do Item", text_color="black", background_color="lavender blush2")
+input_box = sg.InputText(tooltip="Enter a todo item", key="todo")
 button = sg.Button('Add', button_color="black")
-window = sg.Window('MY TO_DO APP', layout=[[label], [input_box, button]], background_color="light coral")
-window.read()
+window = sg.Window('MY TO_DO APP', layout=[[label], [input_box, button]],
+                   background_color="gray20",
+                   font=('Dina', 13))
+sg.TITLEBAR_MAXIMIZE_KEY
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            todos = get_todos()
+            print(todos)
+            new_todo = values['todo'] + '\n'
+            print(new_todo)
+            todos.append(new_todo)
+            write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
 
 def get_todos():
-    with open("../pythonProject3/todos.txt", 'r') as local_file:
+    with open("todos.txt", 'r') as local_file:
         local_todos = local_file.readlines()
     return local_todos
 
 
 def write_todos(todos_args):
-    with open("../pythonProject3/todos.txt", 'w') as local_file:
+    with open("todos.txt", 'w') as local_file:
         local_file.writelines(todos_args)
